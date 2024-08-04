d8f603483b54a9441e99d0b9ed3bf80eb1dca5a7
Leonardo <leonardobastosmoraes@hotmail.com>
2024-08-04 08:00:51 -0300

    first commit

---
+++ src/main/java/info/moraes/cervejariaacmeprojetobloco/CervejariaAcmeProjetoBlocoApplication.java
@@ -0,0 +1,13 @@
+package info.moraes.cervejariaacmeprojetobloco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CervejariaAcmeProjetoBlocoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CervejariaAcmeProjetoBlocoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/info/moraes/cervejariaacmeprojetobloco/CervejariaAcmeProjetoBlocoApplicationTests.java
@@ -0,0 +1,13 @@
+package info.moraes.cervejariaacmeprojetobloco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CervejariaAcmeProjetoBlocoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
